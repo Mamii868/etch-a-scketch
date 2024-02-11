fe07d10f1c100423f029294883fb7ec793b44706
Mamii868 <marcmotenii@gmail.com>
2024-02-11 14:30:36 -0600

    Add logic to color boxes

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const sketchContainer = document.querySelector(".sketchContainer");
 const colorContainer = document.querySelector(".colorContainer");
 
+// creates flex boxes
 function createGrid() {
     for (let i = 1; i <= 256; i++) {
-        sketchContainer.innerHTML += `<div class="sketchBox">${i}</div>`
+        sketchContainer.innerHTML += `<div class="sketchBox"></div>`
     }
 }
-
 createGrid();
+const sketchBox = document.querySelectorAll(".sketchBox");
+
+//captures where mouse hovers
+sketchBox.forEach(box => box.addEventListener('mouseover', () => {
+    box.classList.add('blue')
+}))
