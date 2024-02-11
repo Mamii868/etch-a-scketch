d8d705c7d7c9a5680d76a3aa37496dc01273abc7
Mamii868 <marcmotenii@gmail.com>
2024-02-11 14:56:16 -0600

    Display chosen color on grid

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 const sketchContainer = document.querySelector(".sketchContainer");
 const colorContainer = document.querySelector(".colorContainer");
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink', 'black']
+const colors = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+  "white",
+  "black",
+];
 let currentColor;
-const background = `style="background-color:${currentColor};"`
-// creates flex boxes
+const background = `style="background-color:${currentColor};"`;
+
+// Creates flex boxes
 function createGrid() {
-    for (let i = 1; i <= 256; i++) {
-        sketchContainer.innerHTML += `<div class="sketchBox"></div>`
-    }
+  for (let i = 1; i <= 256; i++) {
+    sketchContainer.innerHTML += `<div class="sketchBox"></div>`;
+  }
 }
 createGrid();
 const sketchBox = document.querySelectorAll(".sketchBox");
 
-//captures where mouse hovers
-sketchBox.forEach(box => box.addEventListener('mouseover', () => {
-    // box.classList.add('blue')
-
-}))
-
+// Captures where mouse hovers
+sketchBox.forEach((box) =>
+  box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = currentColor;
+  })
+);
+// Creates color selection
 function colorGrid() {
-    colors.forEach(color => {
-        colorContainer.innerHTML += `<div class="colorBox" value="${color}" style="background-color:${color}"></div>`
-       
-    })
+  colors.forEach((color) => {
+    colorContainer.innerHTML += `<div class="colorBox" value="${color}" style="background-color:${color}"></div>`;
+  });
 }
-colorGrid()
-const colorBox = document.querySelectorAll(".colorBox")
-colorBox.forEach(box => box.addEventListener("click", () => {
+colorGrid();
+
+// Sets the chosen color
+const colorBox = document.querySelectorAll(".colorBox");
+colorBox.forEach((box) =>
+  box.addEventListener("click", () => {
     currentColor = box.getAttribute("value");
-    console.log(currentColor)
-}))
+    console.log(currentColor);
+  })
+);
