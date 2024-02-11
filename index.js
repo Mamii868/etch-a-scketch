f6ae44034cc9563338754e0d802de46452296054
Mamii868 <marcmotenii@gmail.com>
2024-02-11 14:16:28 -0600

    Add grid using flex boxes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const sketchContainer = document.querySelector(".sketchContainer");
 const colorContainer = document.querySelector(".colorContainer");
 
 function createGrid() {
-    for (let i = 0; i < 256; i++) {
-        sketchContainer.innerHTML += `<div class="sketchBox"></div>`
+    for (let i = 1; i <= 256; i++) {
+        sketchContainer.innerHTML += `<div class="sketchBox">${i}</div>`
     }
 }
+
+createGrid();
