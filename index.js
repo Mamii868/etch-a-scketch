a1851a6b5d08752ae6e0e39d8c2322af5f429fba
Mamii868 <marcmotenii@gmail.com>
2024-02-12 15:21:07 -0600

    Use fragments instead of for loops for grid
    The loops were taking too many resources trying to create the elements and append them at the same time. using fragments allows it to create all elements and append them at once

--- index.js
+++ index.js
@@ -27,12 +27,11 @@ createGrid();
 
 //Finds hovered flex item and changes color
 const sketchBox = document.querySelectorAll(".sketchBox");
-sketchContainer.addEventListener('mouseover', function(e) {
-    if(e.target.classList.contains('sketchBox')) {
-        e.target.style.backgroundColor = currentColor;
-    }
-   
-})
+sketchContainer.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("sketchBox")) {
+    e.target.style.backgroundColor = currentColor;
+  }
+});
 
 // Creates color selection
 function colorGrid() {
@@ -59,49 +58,25 @@ colorBox.forEach((box) =>
 
 editGridBtn.addEventListener("click", () => {
   let size = window.prompt("Choose size");
-  console.log(typeof size)
+  console.log(typeof size);
   if (!Number.isInteger(Number(size))) {
     alert("Please input a valid number!");
   } else {
     if (size < 2 || size > 100) {
       alert("Must be between 2x2 and 100x100");
     } else {
+      const grid = size * size;
+      const pixelsize = 476.8 / size;
+      const frag = document.createDocumentFragment();
+      for(let i = 0; i < grid; i++) {
+        const box = document.createElement('div')
+        box.className = 'sketchBox';
+        box.style.width = `${pixelsize}px`;
+        box.style.height = `${pixelsize}px`;
+        frag.appendChild(box)
+      }
       sketchContainer.innerHTML = ``;
-      let grid = size * size
-      let pixelsize = 476.8 / size;
-
-      if (grid % 8 === 0) {
-        for (let g = 0; g < 8; g++) {
-            setTimeout(() => {
-            for (let i = 1; i <= (grid / 8); i++) {
-                sketchContainer.innerHTML += `<div class="sketchBox" style="flex-basis: ${pixelsize}px; height: ${pixelsize}px;"></div>`;
-              }}, 0)
-          }
-      } else if (grid % 6 === 0) {
-        for (let g = 0; g < 6; g++) {
-            setTimeout(() => {
-            for (let i = 1; i <= (grid / 6); i++) {
-                sketchContainer.innerHTML += `<div class="sketchBox" style="flex-basis: ${pixelsize}px; height: ${pixelsize}px;"></div>`;
-              }
-              }, 0)
-          }
-      } else if (grid % 4 === 0) {
-        for (let g = 0; g < 4; g++) {
-            setTimeout(() => {
-            for (let i = 1; i <= (grid / 4); i++) {
-                sketchContainer.innerHTML += `<div class="sketchBox" style="flex-basis: ${pixelsize}px; height: ${pixelsize}px;"></div>`;
-              }
-              }, 0)
-          }
-      } else {
-        for (let g = 0; g < 2; g++) {
-            setTimeout(() => {
-            for (let i = 1; i <= (grid / 2); i++) {
-                sketchContainer.innerHTML += `<div class="sketchBox" style="flex-basis: ${pixelsize}px; height: ${pixelsize}px;"></div>`;
-              }
-              }, 0)
-          }
-      }     
+      sketchContainer.appendChild(frag);
     }
   }
 });
