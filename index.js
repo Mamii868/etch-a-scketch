2977fe2ce67b28fdb9884039470752ec15c1f686
Mamii868 <marcmotenii@gmail.com>
2024-02-12 14:25:30 -0600

    Add function to edit grid

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const sketchContainer = document.querySelector(".sketchContainer");
 const colorContainer = document.querySelector(".colorContainer");
 const eraseBtn = document.querySelector(".eraseBtn");
+const editGridBtn = document.querySelector(".sizeInput");
 const colors = [
   "red",
   "orange",
@@ -36,13 +37,13 @@ function colorGrid() {
   colors.forEach((color) => {
     colorContainer.innerHTML += `<div class="colorBox" value="${color}" style="background-color:${color}"></div>`;
   });
-  colorContainer.classList.add("colorContainerShrink")
-//Erase Screen
-eraseBtn.addEventListener("click", () => {
-    sketchBox.forEach(box => {
-        box.style.backgroundColor = 'white';
-    })
-})
+  colorContainer.classList.add("colorContainerShrink");
+  //Erase Screen
+  eraseBtn.addEventListener("click", () => {
+    sketchBox.forEach((box) => {
+      box.style.backgroundColor = "white";
+    });
+  });
 }
 colorGrid();
 
@@ -53,3 +54,21 @@ colorBox.forEach((box) =>
     currentColor = box.getAttribute("value");
   })
 );
+
+editGridBtn.addEventListener("click", () => {
+  let size = window.prompt("Choose size");
+  console.log(typeof size)
+  if (!Number.isInteger(Number(size))) {
+    alert("Please input a valid number!");
+  } else {
+    if (size < 2 || size > 100) {
+      alert("Must be between 2x2 and 100x100");
+    } else {
+      sketchContainer.innerHTML = ``;
+      for (let i = 1; i <= (size * size); i++) {
+        sketchContainer.innerHTML += `<div class="sketchBox"></div>`;
+      }
+      
+    }
+  }
+});
